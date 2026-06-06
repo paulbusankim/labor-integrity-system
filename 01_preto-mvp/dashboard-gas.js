dcfd785826a92e912bd9dfb54947716738454aa0
paulbusankim <paulbusankim@gmail.com>
2026-06-06 17:20:09 +0900

    refactor: 시트 호출 로직 공통화 및 유틸리티 함수 분리

--- 01_preto-mvp/dashboard-gas.js
+++ 01_preto-mvp/dashboard-gas.js
@@ -5,8 +5,7 @@ var SUBMIT_CELL = "B14";
 
 // 시트 파일이 열릴 때 자동으로 실행되는 구글 예약 함수입니다.
 function onOpen() {
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = spreadsheet.getSheetByName(DASHBOARD_TAB_NAME);
+  var sheet = getSheet(DASHBOARD_TAB_NAME);
 
   if (!sheet) return;
 
@@ -21,6 +20,11 @@ function onOpen() {
 function onEdit(e) {
   // 등록한 라이브러리 이름(app_Labor_Master_DB)을 통해 마스터 엔진의 함수를 호출하면서 이벤트 매개변수(e)를 안전하게 전달
   app_Labor_Master_DB.saveLogFromUser(e);
+}
+
+function getSheet(tabName) {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  return spreadsheet.getSheetByName(tabName);
 }
 
 function resetCheckbox(sheet, range) {
