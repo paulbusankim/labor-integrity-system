eecc6587498f24ca52ee2c9100119c435985b7c2
paulbusankim <paulbusankim@gmail.com>
2026-06-06 16:38:47 +0900

    refactor: 체크박스 초기화 로직을 별도 함수로 분리

--- 01_preto-mvp/dashboard-gas.js
+++ 01_preto-mvp/dashboard-gas.js
@@ -9,13 +9,17 @@ function onOpen() {
 
   // 만약 계산기 탭이 존재한다면, B10부터 B12까지의 체크박스를 전부 false(꺼짐)로 만듭니다.
   if (sheet) {
-    sheet
-      .getRange(DATA_CHECKBOX_RANGE_START + ":" + DATA_CHECKBOX_RANGE_END)
-      .setValue(false);
+    resetAllCheckbox(sheet);
   }
 }
 
 function onEdit(e) {
   // 등록한 라이브러리 이름(app_Labor_Master_DB)을 통해 마스터 엔진의 함수를 호출하면서 이벤트 매개변수(e)를 안전하게 전달
   app_Labor_Master_DB.saveLogFromUser(e);
+}
+
+function resetAllCheckbox(sheet) {
+  sheet
+    .getRange(DATA_CHECKBOX_RANGE_START + ":" + DATA_CHECKBOX_RANGE_END)
+    .setValue(false);
 }
