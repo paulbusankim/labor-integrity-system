d7064b47d0f287a56400bd036339579a349deb08
paulbusankim <paulbusankim@gmail.com>
2026-07-09 18:29:20 +0900

    refactor: 데이터 매핑 설정 구조 개선
    - USER_CHECKBOX_OPTION_MAP 설정을 getMappingConfig 함수로 추상화하여 관리
    - 매핑 과정에서 유효하지 않은 옵션 필터링 로직 추가를 통해 데이터 처리 안정성 확보

--- 01_preto-mvp/master-db-gas.js
+++ 01_preto-mvp/master-db-gas.js
@@ -1,11 +1,6 @@
 var MASTER_DB_SHEET_URL =
   "https://docs.google.com/spreadsheets/d/1Egi76V4st202iYPAaQqjqHppV0Ej1Wc01FBPsqNVFSc/edit?gid=920517590#gid=920517590";
 var LOG_SHEET_NAME = "Log";
-var USER_CHECKBOX_OPTION_MPA = {
-  B10: "주말/야간 수당 옵션 클릭",
-  B11: "30분 단위 절사 비교 옵션 클릭",
-  B12: "고용노동부 리포트 옵션 클릭",
-};
 
 function saveLogFromUser(payload) {
   var event = payload.event;
@@ -13,11 +8,16 @@ function saveLogFromUser(payload) {
   if (!event) return;
 
   var range = event.range;
-  var sheet = range.getSheet();
-  var cellAddress = range.getA1Notation();
+  var mappingConfig = getMappingConfig();
+  
   var selectedOptions = payload.data
     .filter((item) => item.values === true)
-    .map((item) => USER_CHECKBOX_OPTION_MPA[item.cell]);
+    .map((item) => {
+      var option = mappingConfig[item.cell];
+      if (!option) return null;
+      return option;
+    })
+    .filter((option) => option !== null);
 
   if (selectedOptions.length === 0) return;
 
@@ -42,4 +42,16 @@ function saveLogFromUser(payload) {
 function hasUserSubmitTrue(event) {
   var value = event.value;
   return value === "TRUE" || value === true ? true : false;
+}
+
+function getMappingConfig() {
+  var sheet =
+    SpreadsheetApp.openByUrl(MASTER_DB_SHEET_URL).getSheetByName("Config");
+  var data = sheet.getDataRange().getValues();
+  return data.reduce(function (acc, row) {
+    var cellAddress = row[0];
+    var optionName = row[1];
+    if (cellAddress && optionName) acc[cellAddress] = optionName;
+    return acc;
+  }, {});
 }
