10d2dd66c50881f390214a6886bc64b60d86dd54
paulbusankim <paulbusankim@gmail.com>
2026-07-15 15:11:19 +0900

    refactor: 코드 효율을 위해 _loadMasterFile 삭제
    - 필요한 곳에서는 MASTER_FILE의 인라인 호출로 대체

--- 01_preto-mvp/master-db-gas.js
+++ 01_preto-mvp/master-db-gas.js
@@ -18,16 +18,6 @@ const _makeConfig = (masterFile) => {
   return data.reduce(addCell, {});
 };
 
-/**
- * 설정 파일로부터 매핑 정보를 가져오는 인터페이스 함수입니다.
- * 보안을 위해 실제 설정 시트의 위치를 직접 노출하지 않고 래핑된 형태로 제공합니다.
- * @returns {Object} 셀 주소와 옵션 이름이 매핑된 객체
- */
-const _loadMasterFile = () => {
-  if (!MASTER_FILE) MASTER_FILE = SpreadsheetApp.openByUrl(MASTER_DB_SHEET_URL);
-  return _makeConfig(MASTER_FILE);
-};
-
 function _saveLogFromUser(payload) {
   const TAG = "[MasterDB:saveLog]";
 
@@ -93,7 +83,11 @@ function _saveLogFromUser(payload) {
 
 const MasterDB = {
   saveCheckboxStatus: _saveLogFromUser,
-  getConfig: _loadMasterFile,
+  getConfig: () => {
+    if (!MASTER_FILE)
+      MASTER_FILE = SpreadsheetApp.openByUrl(MASTER_DB_SHEET_URL);
+    return _makeConfig(MASTER_FILE);
+  },
 };
 
 function getConfig() {
